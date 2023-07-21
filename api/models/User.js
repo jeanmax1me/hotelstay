563caaf9139d0ff9a1613da67a3b5cb6d7ecdbed
jeanmax1me <68813864+jeanmax1me@users.noreply.github.com>
2023-07-21 17:55:39 +0200

    Create User.js

---
+++ api/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+});
+
+const UserModel = new mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
