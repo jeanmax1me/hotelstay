b87b88a12541cd58323440f9120bf4484f382470
jeanmax1me <68813864+jeanmax1me@users.noreply.github.com>
2023-07-21 15:20:30 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import IndexPage from "../pages/IndexPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import Layout from "./Layout";
+
+import './App.css'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+
+    </Routes>
+  );
+}
+
+
+export default App
